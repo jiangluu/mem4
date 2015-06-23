c3b08afcc3057d42359f2d37c291f0e8d02ae6d0
jl <jiangluu@users.noreply.github.com>
2015-06-23 04:21:58 +0000

    给redis.command_and_wait()添加参数类型安全检查，堵上漏洞

--- serv/bin64/b/lua/predefine/aredis.lua
+++ serv/bin64/b/lua/predefine/aredis.lua
@@ -53,6 +53,43 @@ function o.command_and_wait(redis_index,formatt,...)
 	local con = o.get_conn(redis_index)
 	assert(nil~=con)
 	
+	-- 做参数类型安全检查
+	--[[
+	local dd = 8
+	local ee = ffi.cast('size_t',dd)
+	print(type(ee))										cdata
+	print(ffi.istype('size_t',ee),'size_t')	true
+	print(ffi.istype('int',ee),'int')				false
+	print(ffi.istype('uint32_t',ee),'uint32_t')	true
+	--]]
+	local pa = { ... }
+	local offset = 0
+	for cc in string.gmatch(formatt,'%%(%a)') do
+		if 'd'==string.lower(cc) then
+			offset = offset+1
+			if not (ffi.istype('size_t',pa[offset]) or ffi.istype('int',pa[offset])) then
+				error('format check failed, need size_t')
+			end
+		elseif 's'==string.lower(cc) then
+			offset = offset+1
+			if not ('string'==type(pa[offset]) or ffi.istype('char*',pa[offset])) then
+				error('format check failed, need string')
+			end
+		elseif 'b'==string.lower(cc) then
+			offset = offset+2
+			local c = pa[offset-1]
+			local lenth = pa[offset]
+			if not ('string'==type(c) or ffi.istype('char*',c)) then
+				error('format check failed, need string and size_t')
+			end
+			if not (ffi.istype('size_t',lenth) or ffi.istype('int',lenth)) then
+				error('format check failed, need string and size_t')
+			end
+		else
+			error('unsupported format '..cc)
+		end
+	end
+	
 	local r = lcf.box_redis_async_command(con,formatt,...)
 	return select(2,transaction.wait())
 end
