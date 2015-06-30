216dfe7a5f02c52d0530605df1133de62b806fcf
jl <jl@0967b8b8-34a7-417b-87fc-a26fa4f5a6d6>
2015-06-30 08:15:24 +0000

    改bug

--- tankgirl/trunk/serv/bin64/b/lua/msg_custom/msg_1001.lua
+++ tankgirl/trunk/serv/bin64/b/lua/msg_custom/msg_1001.lua
@@ -1,9 +1,17 @@
 
--- Offline 1000
+local lcf = ffi.C
+
+-- Offline 1001
 function onMsg(me)
+	
+	local actor_id = tonumber(lcf.cur_actor_id())
+	box.release_actor(actor_id)
+	
+	-- C里的工作 目前只要计数器减一就可以了
+	lcf.box_actor_num_dec(1)
 	
 	return 0
 end
 
 
-box.add_exit_message(1000)
+box.add_exit_message(1001)
