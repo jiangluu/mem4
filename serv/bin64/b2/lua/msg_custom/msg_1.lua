bf06cec276df408cce5ccc31354ff0f74e9df021
jl <jiangluu@users.noreply.github.com>
2015-09-16 10:50:13 +0000

    从b复制来消息handle

--- serv/bin64/b2/lua/msg_custom/msg_1.lua
+++ serv/bin64/b2/lua/msg_custom/msg_1.lua
@@ -1,12 +1,50 @@
 
+local lcf = ffi.C
+
 function onMsg(me)
-	local a = redis.command_and_wait(1,'INCR counter1')
-	print('AAA',a)
+	print('MSG 1 recv')
 	
-	a = redis.command_and_wait(1,'INCR counter1')
-	print('BBB',a)
+	local bin = l_cur_stream_get_slice()							-- 得到客户端发送的字节序列
+	-- local param = pb.decode('SimpleParam',bin)		-- 调用protobuf反序列化
 	
-	l_gx_simple_ack()
+	-- local key = param.string1_							-- 客户端发送的登录key
+	-- local player_data_bin = redis.get(0,key)		-- 从redis读取数据
+	local player_data_bin = nil
+	
+	if nil==player_data_bin then
+		-- 未从数据层读到数据，认为这是个新号，初始化玩家数据
+		local sn = redis.command_and_wait(0,'INCR c_usersn')		-- 得到一个自增长序号
+		
+		me.id_ = 'u'..sn
+		me.name_ = 'guest'..me.id_
+		me.exp_ = 88
+		me.lv_ = 1
+		me.diamond_ = 100
+		
+		local bag = {}
+		table.insert(bag,{ item_id_=2 })
+		table.insert(bag,{ item_id_=3 })
+		
+		me.bag_ = bag
+		-- 初始化玩家数据结束
+		
+		local bin2 = pb.encode('User',me)				-- 调用protobuf序列化
+		lcf.cur_stream_push_string(bin2,#bin2)		-- 放入发送缓冲
+		lcf.cur_stream_write_back()							-- 返回消息给客户端，消息ID是1+1，内容是放入发送缓冲的字节
+		
+	else
+		-- 读到了以前的存盘数据
+		
+		local player_data = pb.decode('User',player_data_bin)		-- 调用protobuf反序列化
+		
+		for k,v in pairs(player_data) do		-- 遍历数据，复制到me上
+			me[k] = v
+		end
+		
+		lcf.cur_stream_push_string(player_data_bin,#player_data_bin)		-- 放入发送缓冲
+		lcf.cur_stream_write_back()																	-- 返回消息给客户端，消息ID是1+1，内容是放入发送缓冲的字节
+	end
 	
 	return 0
 end
+
--- serv/bin64/b2/lua/msg_custom/msg_11.lua
+++ serv/bin64/b2/lua/msg_custom/msg_11.lua
@@ -1,15 +1,12 @@
 
-math.randomseed(os.time())
+-- HeartBeat
 
-function onMsg(me)
+local lcf = ffi.C					-- LuaJit相关，可以不用理解，照抄即可
+
+function onMsg(me)		-- 函数定义，此目录下每个消息的定义都一样，照抄即可
+	print('HeartBeat')
 	
-	local rn = math.random(100)
-	if rn<=5 then
-		local a = redis.command_and_wait(1,'INCR counter1')
-		print('msg11',a)
-	end
-	
-	l_gx_simple_ack()
+	lcf.cur_stream_write_back()			-- 返回消息给客户端，消息ID是11+1
 	
 	return 0
 end
