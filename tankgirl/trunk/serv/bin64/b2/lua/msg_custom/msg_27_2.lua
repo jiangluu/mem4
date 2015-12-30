8fba85136e0d3cd02717ea3ef9ef4adeb864e9f2
jl <jl@0967b8b8-34a7-417b-87fc-a26fa4f5a6d6>
2015-12-30 07:23:00 +0000

    清调教CD

---
+++ tankgirl/trunk/serv/bin64/b2/lua/msg_custom/msg_27_2.lua
@@ -0,0 +1,44 @@
+
+local lcf = ffi.C
+
+function onMsg(me,merge_meta,merge)
+	local hero_id = lcf.cur_stream_get_int32() 
+	
+	
+	local hero_i = -1
+	for i=1,#me.heroes do
+		if hero_id == me.heroes[i].id then
+			hero_i = i
+			break
+		end
+	end
+	
+	if hero_i<0 then
+		return 2
+	end
+	
+	
+	local nan = me.heroes[hero_i]
+	local now = now()
+	if (nan.dis_cd or 0) <= now then
+		return 3
+	end
+	
+	local cost = 100
+	
+	if not bag.dec(me,10002,cost,'clear_dis_cd') then
+		return 4
+	end
+	
+	nan.dis_cd = now - 1
+	
+	
+	local me2 = {}
+	me2.diamond = me.diamond
+	me2.heroes = { nan }
+	table.insert(merge_meta,'User')
+	table.insert(merge,me2)
+	
+	
+	return 0
+end
